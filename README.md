a5d55cd92f1b9a06dc563f8ac6c5e905eec53d97
DavidHuang <david.huang3214@gmail.com>
2023-06-30 10:59:00 -0700

    Add turn taking and displaying markers on game board

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Tic Tac Toe minigame.
 JavaScript practice project for [The Odin Project](https://www.theodinproject.com/).
 
 CSS template provided by [Skeleton](http://getskeleton.com/).
+
+# Learning points
+
+- Structuring JavaScript with Factory functions & Modules 
+- Refactoring code to reduce global namespace clutter
+- Linking DOM changes between two Modules (gameBoard and displayController)
--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,72 @@
+const playerFactory = (name, marker) => {
+    return { name, marker };
+};
 
 const gameBoard = (() => {
-    let board  = [
-        'X', 'O', 'X', 
-        'O', 'X', 'O', 
-        'X', 'O', 'X'
-        ];
+    let board = [ 
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ];
 
-    const changeTile = (tileIndex, marker) => {
-        if (tileIndex < 0 || tileIndex > 8) return;
-        if (marker === 'O' || marker === 'X') board[tileIndex] = marker;
-    }
-    const getBoard = () => {return board};
+    const player1 = playerFactory('Player 1', 'X');
+    const player2 = playerFactory('Player 2', 'O');
+    let playerTurn = player1;
+    // const coinFlip = Math.floor(Math.random() * 2 + 1);
+    // const startingPlayer = coinFlip === 1 ? player1 : player2;
 
-    return {changeTile, getBoard};
+    const getBoard = () => board;
+    const getPlayerTurn = () => playerTurn;
+    const resetBoard = () => {
+        for (let index = 0; index < board.length; index++) {
+            board[index] = '';
+        }
+    };
+
+    const isTileEmpty = (tileIndex) => board[tileIndex] ? false : true;
+    const changeTile = (tileIndex, marker) => board[tileIndex] = marker;
+
+    const takeTurn = (tileIndex) => {
+        console.log(playerTurn.name);
+        if(!isTileEmpty(tileIndex)){
+            return;
+        }
+        else{
+            changeTile(tileIndex, playerTurn.marker);
+        }
+        
+        playerTurn = playerTurn === player1 ? player2 : player1;
+        console.log('selected tile ' + tileIndex);
+    };
+
+    return { 
+        getBoard,
+        getPlayerTurn,
+        resetBoard, 
+        isTileEmpty, 
+        changeTile,  
+        takeTurn };
 })();
+
 
 const displayController = (() => {
     const game_container = document.querySelector('.game-container');
-
-    const initialBoardSetup = () => {
+    const turn_display = document.querySelector('.turn-display');
+    
+    const initializeBoard = () => {
         for (let index = 0; index < 9; index++) {
             const tile = document.createElement('p');
             tile.setAttribute('data-index', index);
             tile.classList.add('square');
+
+            tile.addEventListener('click', () => {
+                gameBoard.takeTurn(tile.getAttribute('data-index'));
+                displayBoard();
+            });
+
             game_container.appendChild(tile);
         }
-    }
+    };
 
     const displayBoard = () => {
         const board = gameBoard.getBoard();
@@ -34,10 +75,11 @@ const displayController = (() => {
             const tile = tiles[index];
             tile.textContent = board[index];
         }
-    }
+        turn_display.textContent = gameBoard.getPlayerTurn().name + " turn";
+    };
 
-    return {initialBoardSetup, displayBoard};
+    return { initializeBoard, displayBoard};
 })();
 
-displayController.initialBoardSetup();
+displayController.initializeBoard();
 displayController.displayBoard();
