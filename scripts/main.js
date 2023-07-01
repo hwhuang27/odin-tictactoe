504187faba0e43021394fb5bff7318c3bbaf8efd
DavidHuang <david.huang3214@gmail.com>
2023-06-30 21:55:03 -0700

    Refactor code to include game states reflected by textDisplay(), add restart button

--- scripts/main.js
+++ scripts/main.js
@@ -16,24 +16,49 @@ const gameBoard = (() => {
     // const startingPlayer = coinFlip === 1 ? player1 : player2;
 
     const getBoard = () => board;
+
     const getcurrentPlayer = () => currentPlayer;
+
     const isTileEmpty = (tileIndex) => !board[tileIndex] ? true : false;
-    const changeTile = (tileIndex, marker) => board[tileIndex] = marker;
+
+    const changeTile = (tileIndex, marker) => board[tileIndex] = marker
+    ;
     const takeTurn = (tileIndex) => {
-        console.log(currentPlayer.name);
         if(!isTileEmpty(tileIndex))
             return;
         else{
             changeTile(tileIndex, currentPlayer.marker);
         }
-        checkWinner();
-        console.log('selected tile ' + tileIndex);
-        currentPlayer = currentPlayer === player1 ? player2 : player1;
-        
-    };
-    const checkWinner = () => {
-        const marker = currentPlayer.marker;
 
+        const gameState = checkState();
+
+        if (gameState === 'Continue'){
+            currentPlayer = currentPlayer === player1 ? player2 : player1;
+            displayController.displayBoard();
+            displayController.displayText('next turn');
+        }
+        else if (gameState === 'Player 1' || gameState === 'Player 2'){
+            displayController.displayBoard();
+            displayController.displayText('winner');
+            freezeGame();
+
+        }
+        else if (gameState === 'Draw'){
+            displayController.displayBoard();
+            displayController.displayText('draw');
+            freezeGame();
+        }
+    };
+
+    const freezeGame = () => {
+        for (let index = 0; index < board.length; index++) {
+            if(board[index] === ''){
+                board[index] = ' ';
+            }
+        }
+    }
+    const checkState = () => {
+        const marker = currentPlayer.marker;
         if ((board[0] === marker && board[3] === marker && board[6] === marker) ||
             (board[1] === marker && board[4] === marker && board[7] === marker) ||
             (board[2] === marker && board[5] === marker && board[8] === marker) ||
@@ -43,16 +68,14 @@ const gameBoard = (() => {
             (board[0] === marker && board[4] === marker && board[8] === marker) ||
             (board[2] === marker && board[4] === marker && board[6] === marker))
         {
-            console.log(currentPlayer.name + ' wins');
-
+            return currentPlayer.name;
         }
-        else if (!board.includes('')){
-            console.log('board full - tie');
-
-        }
+        else if (!board.includes('')) return 'Draw';
+        else return 'Continue';
     };
 
     const resetBoard = () => {
+        currentPlayer = player1;
         for (let index = 0; index < board.length; index++) {
             board[index] = '';
         }
@@ -64,14 +87,14 @@ const gameBoard = (() => {
         isTileEmpty,
         changeTile,
         takeTurn,
-        checkWinner,
-        resetBoard
+        checkState,
+        resetBoard,
      };
 })();
 
 const displayController = (() => {
     const game_container = document.querySelector('.game-container');
-    const turn_display = document.querySelector('.turn-display');
+    const text_display = document.querySelector('.text-display');
     
     const initializeBoard = () => {
         for (let index = 0; index < 9; index++) {
@@ -81,7 +104,6 @@ const displayController = (() => {
 
             tile.addEventListener('click', () => {
                 gameBoard.takeTurn(tile.getAttribute('data-index'));
-                displayBoard();
             });
 
             game_container.appendChild(tile);
@@ -95,10 +117,32 @@ const displayController = (() => {
             const tile = tiles[index];
             tile.textContent = board[index];
         }
-        turn_display.textContent = gameBoard.getcurrentPlayer().name + " turn";
     };
 
-    return { initializeBoard, displayBoard};
+    const displayText = (action) =>{
+        switch (action){
+            case 'next turn':
+                text_display.textContent = gameBoard.getcurrentPlayer().name + " turn";
+                break;
+            case 'winner': 
+                text_display.textContent = `${gameBoard.getcurrentPlayer().name} wins!`;
+                break;
+            case 'draw':
+                text_display.textContent = `It's a draw!`;
+        }
+    }
+    
+    const restart_btn = document.querySelector('.restart-btn');
+    restart_btn.addEventListener('click', () => {
+        displayReset();
+    });
+    const displayReset = () => {
+        gameBoard.resetBoard();
+        text_display.textContent = gameBoard.getcurrentPlayer().name + " turn";
+        displayBoard();
+    }
+
+    return { initializeBoard, displayBoard, displayText, displayReset};
 })();
 
 displayController.initializeBoard();
